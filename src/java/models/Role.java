9a5cd977fa8eefd8c2c0a1169db69b158316ba66
User <User@Barbra-Manatee>
2022-07-18 12:20:24 -0600

    Part B done

---
+++ src/java/models/Role.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.io.Serializable;
+
+public class Role implements Serializable{
+    
+    private int rID;
+    private String rName;
+
+    public Role() {
+    }
+
+    public Role(int rID, String rName) {
+        this.rID = rID;
+        this.rName = rName;
+    }
+
+    public int getrID() {
+        return rID;
+    }
+
+    public void setrID(int rID) {
+        this.rID = rID;
+    }
+
+    public String getrName() {
+        return rName;
+    }
+
+    public void setrName(String rName) {
+        this.rName = rName;
+    }
+    
+}
---
+++ src/java/models/User.java
@@ -0,0 +1,76 @@
+package models;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    
+    private String email;
+    private String active;
+    private String fName;
+    private String lName;
+    private String password;
+    private int role;
+
+    public User() {
+    }
+
+    public User(String email, String active, String fName, String lName, String password, int role) {
+        this.email = email;
+        this.active = active;
+        this.fName = fName;
+        this.lName = lName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+    
+    
+    
+}
